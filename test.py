87cd7693d4078151d0d31d9e327e15c899974a74
Zachary Yaro <zmyaro@gmail.com>
2013-12-18 00:21:12 -0500

    Switch to Python 2.7 and make KanjiTestZ use the webapp framework properly

--- test.py
+++ test.py
@@ -1,4 +1,22 @@
-kanjiTestFile = open('test.html')
-kanjiTestCode = kanjiTestFile.read()
-kanjiTestFile.close()
-print kanjiTestCode
+
+import cgi
+import os
+import urllib
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class KanjiTestPage(webapp.RequestHandler):
+	def get(self, stuff): # Ignore any stuff after the base URL
+		path = os.path.join(os.path.dirname(__file__), 'test.html')
+		self.response.out.write(template.render(path, {}))
+
+
+site = webapp.WSGIApplication([('/kanjitest(/.*)?', KanjiTestPage)], debug=True)
+
+def main():
+	run_wsgi_app(site)
+
+if __name__ == '__main__':
+	main()
