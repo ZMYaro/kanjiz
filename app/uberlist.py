1cedc5349dcb6b038478368d469f5084de25b55c
Zachary Yaro <zmyaro@gmail.com>
2014-01-20 01:19:30 -0500

    Update KanjiList to webapp2

--- app/uberlist.py
+++ app/uberlist.py
@@ -1,4 +1,20 @@
-uberListFile = open('uberlist.html')
-uberListCode = uberListFile.read()
-uberListFile.close()
-print uberListCode
+import os
+
+import jinja2
+import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+	loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates/')),
+	extensions=['jinja2.ext.autoescape'],
+	autoescape=True)
+
+class KanjiListPage(webapp2.RequestHandler):
+	def get(self, stuff): # Ignore any stuff after the base URL
+		template = JINJA_ENVIRONMENT.get_template('uberlist.html')
+		self.response.write(template.render({}))
+
+
+app = webapp2.WSGIApplication([
+	('/kanjilist(/.*)?', KanjiListPage),
+	('/uberlist(/.*)?', KanjiListPage)
+], debug=True)
